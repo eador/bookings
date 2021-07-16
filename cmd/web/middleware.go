f95d5100c979236bc7c971b883e733bf5340f178
Corey <corey.slate@softvision.com>
2021-07-16 14:33:25 -0600

    Created a secure page

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -29,7 +29,7 @@ func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !helpers.IsAuthenticated(r) {
 			session.Put(r.Context(), "error", "Log in first!")
-			http.Redirect(w, r, "/user/logn", http.StatusSeeOther)
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 			return
 		}
 		next.ServeHTTP(w, r)

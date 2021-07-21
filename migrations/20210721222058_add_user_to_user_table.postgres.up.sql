72c422f138acca30f0242788fe19b7b48eb7e667
Corey <corey.slate@softvision.com>
2021-07-21 16:24:15 -0600

    added users content migration

---
+++ migrations/20210721222058_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Corey','Slate','corey@slate.com','$2a$12$rXHxFs5OwafBQ0cghBzzAuVgUIwdVgDDD3l19lbrectpTWyiO6qWu',3,'2020-12-12 00:00:00','2020-12-12 00:00:00');
